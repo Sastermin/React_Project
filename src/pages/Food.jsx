c1f9f9d37f6554ab2a491f6ff319367de461a37b
Dario Tovar <alex101saster@gmail.com>
2026-04-13 18:51:59 -0600

    refactor(Food): extraer datos de productos a JSON en carpeta data

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -1,28 +1,9 @@
 // Importamos la tarjeta reutilizable
 import MovieCard from "../componentes/MovieCard"
+import productos from "../data/productos_comida.json"
 import "../styles/Food.css"
 
 function Food({ verDetalle }) {
-  const productos = [
-    {
-      id: 201,
-      titulo: "Palomitas",
-      imagen: "https://www.gastronosfera.com/sites/default/files/styles/wide/public/trending/2023/palomitas.jpg.webp",
-      descripcion: ""
-    },
-    {
-      id: 202,
-      titulo: "Refresco",
-      imagen: "https://ams3.digitaloceanspaces.com/graffica/2023/02/la-ola-de-cocacola.png",
-      descripcion: ""
-    },
-    {
-      id: 203,
-      titulo: "Dulces",
-      imagen: "https://preview.redd.it/whats-your-go-to-candy-to-bring-to-buy-at-a-movie-theater-v0-bnja1b8vjgde1.jpeg?auto=webp&s=0a8b56987039cc5ff965e960271e92f375b5c68a",
-      descripcion: ""
-    }
-  ]
 
   return (
     <main className="grid-layout">
